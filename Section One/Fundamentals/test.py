9211260dda1284f6b40025a8251180961e423083
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-22 23:46:27 +0530

    Fundamentals

--- Section One/Fundamentals/test.py
+++ Section One/Fundamentals/test.py
@@ -17,31 +17,31 @@ print(type(PI))
 #Arithmetic Operators
 a = 10
 b = 5
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a**b)
+print(a+b) # 15
+print(a-b) # 5
+print(a*b) # 50
+print(a/b) # 2.0
+print(a%b) # 0
+print(a**b) # 100000
 
 # Relational Operators
-print(a > b)
-print(a >= b)
-print(b <= a)
-print(a == b)
-print(a!=b)
+print(a > b) # True
+print(a >= b) # True
+print(b <= a) # True
+print(a == b) # False
+print(a!=b) # True
 
 # Assignment Operators
 c = 2
 c+= 2
-print(c)
+print(c) # 4
 d = 10
 d *= 2
-print(d)
+print(d) # 20
 d -= 5
-print(d)
+print(d) # 15
 c%= 2
-print(c)
+print(c) # 0
 
 # Logical Operators
 #not , and, or
