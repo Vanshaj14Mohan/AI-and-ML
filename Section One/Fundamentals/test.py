7e7c419c37cd82f6c409d482e0762e55ddcb0398
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-03-23 21:30:02 +0530

    Fundamentals

--- Section One/Fundamentals/test.py
+++ Section One/Fundamentals/test.py
@@ -1,3 +1,5 @@
+# PYTHON FUNDAMENTALS
+
 print("hello world")
 print("AI/ML batch \n2.0")
 
@@ -56,3 +58,26 @@ print((4 >3) and (7 > 5)) # True
 #Using or
 print((4 >3) or (7 >9)) # True
 print((4> 5) or (5>7)) # False
+
+# Type conversion and Type Casting
+ans1 = int(10 + 9.0)
+ans2 = 5 + 10.0
+print(ans1, type(ans1)) # 19 <class 'int'>
+print(ans2, type(ans2)) # 15.0 <class 'float'>
+
+test_one = bool(0)
+print(test_one, type(test_one)) # False <class 'bool'>
+test = bool(10) # Except 0 in boolean everything is True
+print(test, type(test)) # True <class 'bool'>
+
+# Taking User Input
+user1 = int(input("Enter user1 id: "))
+print(user1)
+user2 = int(input("Enter user2 id: "))
+print(user2)
+
+# Average of two numbers
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+avg = (num1+num2)/2
+print("Average is: ", avg)
